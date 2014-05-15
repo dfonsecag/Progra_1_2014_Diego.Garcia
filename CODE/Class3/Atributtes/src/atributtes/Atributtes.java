ff1b93505311bbdf90ab2d0eddefd39a613028a1
dfonsecag <dgf-95@hotmail.com>
2014-05-15 11:21:37 -0600

    Clase3

--- CODE/Class3/Atributtes/src/atributtes/Atributtes.java
+++ CODE/Class3/Atributtes/src/atributtes/Atributtes.java
@@ -4,6 +4,8 @@
  */
 package atributtes;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Estudiante
@@ -14,6 +16,53 @@ public class Atributtes {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        boolean validar= true;
+        int opcion;
+        double monto;
+        char continuar;
+        
+        account oAccount = new  account();
+             
+        Scanner teclado = new Scanner(System.in);
+        
+        while(validar)
+        {
+            
+            System.out.println("Digite 1si quiere realizar un deposito///2. Si quiere realizar un retiro");            
+            opcion = Integer.parseInt(teclado.nextLine());
+            
+            if (opcion==1)
+            {
+                System.out.println("DIGITE EL MONTO A DEPOSITAR");
+               monto = Double.parseDouble(teclado.nextLine());                
+                oAccount.Deposito(monto);
+            }
+            else if (opcion==2)
+            {
+                System.out.println("DIGITE EL MONTO A RETIRAR");
+                monto = Double.parseDouble(teclado.nextLine());                
+                oAccount.reTIRO(monto);
+                if(oAccount.isHayerror())
+                {
+                    System.out.println("CUENTA INSUFICIENTE");
+                }
+                else
+                {
+                    
+                }
+            }
+            System.out.println("EL SALDO FINAL ES DE "+ oAccount.getSaldoInicial());
+            System.out.println("DESEA CONTINUAR CON OTRA TRANSACCION S/N");
+            continuar = teclado.nextLine().charAt(0);
+              if (continuar=='s'|| continuar== 'S')
+              {
+                  validar= true;
+              }
+              else
+              {
+                  validar = false;
+              }
+        }      
+      
     }
 }
--- CODE/Class3/Atributtes/src/atributtes/account.java
+++ CODE/Class3/Atributtes/src/atributtes/account.java
@@ -9,5 +9,53 @@ package atributtes;
  * @author Estudiante
  */
 public class account {
+   private double saldoInicial;
+
+   public boolean  hayerror;
     
+    public account()
+    {
+        saldoInicial=0;
+        hayerror = false;
+    }
+    /*
+     * Este metodos irve oara aumantar la cuenta del usuario..
+     * @ param monto Parametro para recibir el metodo...
+     */
+    public void Deposito(double Pmonto)
+    {
+        setSaldoInicial(getSaldoInicial()+Pmonto);
+    }
+    /*
+     * Este metodos irve oara disminuir la cuenta del usuario..
+     * @ param monto Parametro para recibir el metodo...
+     */
+    public void reTIRO(double  Pmonto)
+    {
+        if (getSaldoInicial()>=Pmonto)
+        {
+            setSaldoInicial(getSaldoInicial()-Pmonto);
+            setHayerror(false);
+        }
+        else
+        {
+            setHayerror(true);
+        }
+    }
+    
+    public double getSaldoInicial() {
+        return saldoInicial;
+    }
+
+    public void setSaldoInicial(double saldoInicial) {
+        this.saldoInicial = saldoInicial;
+    }
+
+    public boolean isHayerror() {
+        return hayerror;
+    }
+
+    public void setHayerror(boolean hayerror) {
+        this.hayerror = hayerror;
+    }
 }
