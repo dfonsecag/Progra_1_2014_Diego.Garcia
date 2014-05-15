0337579244ebf54c2a3c976b8fb26c30d4709760
dfonsecag <dgf-95@hotmail.com>
2014-05-15 09:23:01 -0600

    class 3

---
+++ CODE/Nueva Carpeta/ATTRIBUTES/src/attributes/ATTRIBUTES.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package attributes;
+
+/**
+ *
+ * @author Estudiante
+ */
+public class ATTRIBUTES {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
